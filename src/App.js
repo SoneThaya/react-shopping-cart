e90e21cedb696d6705e4182f324655f9e819b400
Sone <sthaya@gmail.com>
2020-05-14 14:11:25 -0700

    created cartContext, components consuming from provider now.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -19,16 +22,25 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+		<CartContext.Provider value={{cart}}>
+			<Navigation />
+				{/*<Navigation cart={cart} />*/}
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/">
+					<Products />
+					{/*	<Products products={products} addItem={addItem} /> */}
+				</Route>
+			</ProductContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			
+				<Route path="/cart">
+					<ShoppingCart value={{cart}} />
+					{/*<ShoppingCart cart={cart} />*/}
+				</Route>
+			</CartContext.Provider>
+
 		</div>
 	);
 }
